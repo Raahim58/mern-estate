939ced74a94f57dcf2277d78eacd8b71839ec5e9
Raahim098 <27100419@lums.edu.pk>
2024-05-31 16:04:35 +0500

    create a middleware and a function to handle possible errors

--- server/index.js
+++ server/index.js
@@ -31,3 +31,12 @@ connectDB();
 app.use("/client/user", userRouter);
 app.use('/client/auth', authRouter);
 
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal server error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
